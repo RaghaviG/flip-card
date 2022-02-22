270941473df7aca9f9354e67bd95340f194cc2c1
RaghaviG <41799200+RaghaviG@users.noreply.github.com>
2022-02-22 20:23:35 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,49 +1,86 @@
 import logo from './logo.svg';
 import './App.css';
 import react,{useState,useEffect} from 'react';
+import Flipsound from './sounds/flip.mp3';
+import Shuffle from './sounds/shuffle.mp3';
+import Matchedsound from './sounds/matched.mp3';
+import Winning from './sounds/winning.mp3';
+import {Howl, Howler} from 'howler';
+
 
 function App() {
 
   const pictures =[
-  {backFace:require("./images/banana.png"),hide:false},
+  {backFace:require("./images/pancake.png"),hide:false},
   {backFace:require("./images/burger.png"),hide:false},
   {backFace:require("./images/muffin.png"),hide:false},
   {backFace:require("./images/pizza.png"),hide:false},
   {backFace:require("./images/rice.png"),hide:false},
+  {backFace:require("./images/eggs.png"),hide:false},
+  {backFace:require("./images/icecream.png"),hide:false},
   {backFace:require("./images/spaghetti.png"),hide:false},
+  {backFace:require("./images/sandwich.png"),hide:false},
+  {backFace:require("./images/taco.png"),hide:false}
 ];
 
+// Setup the new Howl.
+const soundplay = (src) => {
+const sound = new Howl({
+  src
+});
+// Play the sound.
+sound.play();
+
+}
+
+// Change global volume.
 
 
 const [cards, setCards] = useState([]);
 const [turns, setTurns] = useState(0);
 const [firstValue,setFirstValue] =  useState(null);
 const [secondValue,setSecondValue] = useState(null);
+const [gameStart,setGameStart] = useState(false);
 const [disabled, setDisabled] = useState(false);
 const [currentID,setCurrentID] = useState(-1);
 const [prevID,setprevID] = useState(-1);
-const [flipped,setFlipped] =  useState(false);
-const shuffleCards = () => {
-  const shuffledCards = [...pictures, ...pictures].sort(() => Math.random()-0.5).map((card) => ({...card,id:Math.random()}))
+const [showpopups,setShowpop] =  useState(false);
+const [score,setScore]= useState(0);
+const [difficulty,setDifficulty] = useState(0);
+const [hideModal,setHideModal] = useState(true);
 
 
+
+const shuffleCards = (cardNumbers) => {
+  soundplay(Shuffle);
+  setGameStart(true);
+  setDifficulty(cardNumbers);
+  //set the difficulty here
+  const pictureCount= pictures.slice(0,cardNumbers)
+  const shuffledCards = [...pictureCount, ...pictureCount].sort(() => Math.random()-0.5).map((card) => ({...card,id:Math.random()}))
   setCards(shuffledCards);
-  console.log(cards);
   setTurns(0);
+  setScore(0);
 }
 
 useEffect(()=>{
-  
-  
+
   if(firstValue && secondValue)
   {
-   
-    console.log('first value:'+firstValue.backFace+" second value:"+secondValue.backFace);
     setDisabled(true);
     if(firstValue.backFace === secondValue.backFace)
-      {console.log("match");
+      {
       clearTimeout();
+      checkScore();
+      //checkflipped();
+     
       resetTurn();
+    soundplay(Matchedsound);
+    if( cards.every(checkflipped))
+     {
+       soundplay(Winning);
+        setHideModal(false);
+     }
     }
     else{
       setTimeout(() => flipbackCard(prevID,false),1000);
@@ -51,21 +88,36 @@ useEffect(()=>{
       resetTurn();
     }
   }
-},[firstValue,secondValue])
+},[firstValue,secondValue]);
+
+ const checkflipped = (values) => {
+  //console.log('for every' + values.hide);
+  return values.hide === true;
+  
+}
+const checkScore = () => {
+ 
+  setScore((score) => score+1);
+ 
+}
+
+
 
 const handleClick = (cardValue,index) => {
+  soundplay(Flipsound);
 flipbackCard(index,true);
- //console.log(index);
- ///setCurrentID(index);
+
 prevID < 0 ? setprevID(index) : setCurrentID(index);
 firstValue ? setSecondValue(cardValue) : setFirstValue(cardValue); 
 
 }
 
 
+
+
 const flipbackCard = (cardID,flipValue) => {
   
-  {console.log('inside flip back card for :' + cardID + ', '+flipValue)
+  
   setCards(updateCard => {
     return updateCard.map((card,id) => {
       if(id === cardID){
@@ -75,33 +127,44 @@ const flipbackCard = (cardID,flipValue) => {
         return card;
       }
     })
-  })}
-}
-const flipCard = (cardToFlip) =>{
-  setCards(prevCard=>{
-    return prevCard.map(card=>{
-      if(card.backFace === cardToFlip.backFace){
-        return{...card,hide:true}
-      }
-      else{
-        return card;
-      }
-    })
   })
 }
+
 const resetTurn = () => {
   setFirstValue(null);
   setSecondValue(null);
   setCurrentID(-1);
   setprevID(-1);
   setTurns((turns)=>turns+1);
+  
 }
+
+Howler.volume(0.5);
   return (
   <>
-  <h1 className='title'>Flip the cards</h1>
-    <button className='button' onClick={shuffleCards}>New Game</button>
-  <section className="memory-game">
+   
+
+  <section className="memory-header">
+   
+
   
+  <h1 className='title'>Flip the cards</h1>
+  <section hidden={difficulty>0  ? false: true}>
+  <button className='btn-rese'  onClick={()=>{return window.location.reload(true);}} >New game</button> 
+  
+    </section>
+
+
+
+    </section> 
+    
+    <section className="memory-game">
+    <div hidden={gameStart}>
+  
+  <button className="glow-on-hover"  onClick={()=>shuffleCards(6)}>Easy</button>
+   <button className="glow-on-hover" onClick={()=>shuffleCards(8)}>Medium</button> 
+   <button className="glow-on-hover" onClick={()=>shuffleCards(10)}>Difficult</button> 
+   </div>
    { cards.map((dat,index)=> { 
     
     return  (<div  key={index} className="memory-card" >
@@ -110,14 +173,19 @@ const resetTurn = () => {
       
       src={dat.hide  ? dat.backFace : require("./images/dish.png")} 
       onClick={()=>handleClick(dat,index)} alt="JS Badge" />
-      {/* <img  className="back-face"  src={require("./images/dish.png")}    
-      onClick={()=> handleClick(dat,index)} hidden={ dat.hide} alt="JS Badge" />
-       */}
-    </div>)})}
+ 
   
+    </div>)})}
+    <div hidden={hideModal}>
+  <h2>Wow! Your score is {Math.floor((score*20)/turns)} out of 20</h2>
+</div>
   </section>
+
   </>
   );
 }
 
 export default App;
+
+
+
